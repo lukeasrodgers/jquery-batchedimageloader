2f90a0acf2ba37ffe7e1a7c1e1c26b4ceb2d62aa
Luke Rodgers <lukeasrodgers@gmail.com>
2012-11-14 13:34:59 -0500

    Initial commit.

---
+++ jquery.batchedimageloader.js
@@ -0,0 +1,44 @@
+(function($) {
+  'use strict';
+  $.fn.batchedImageLoader = function(opts) {
+    var defaults = {
+      delay: 1000,
+      batchSize: 10,
+      className: 'batched-image-loader'
+    };
+    var options = $.extend({}, defaults, opts);
+    var i = 0,
+        batchSize = options.batchSize;
+
+    var process_batch = function(batch) {
+      batch.each(function() {
+        var $el = $(this),
+            el = $el.get(0),
+            loaded = $el.data('imgLoaded');
+        if (!loaded) {
+          el.src = $el.data('imgSrc');
+          $el.data('imgLoaded', true);
+          $el.removeClass('batched-image-loader');
+          el.onerror = function(img) {
+            img.onerror = null;
+          };
+        }
+      });
+    };
+
+    var timeout_batch = function(batch, i) {
+      window.setTimeout(function() {
+        process_batch(batch);
+      }, options.delay * i);
+    };
+
+    while (true) {
+      var batch = this.slice(i * batchSize, batchSize + (i * batchSize));
+      timeout_batch(batch, i);
+      if (batch.length < 10) {
+        break;
+      }
+      i++;
+    }
+  };
+}(jQuery));
